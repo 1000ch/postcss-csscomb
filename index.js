6da0926fcde6104b48d5e1155dc8c21d419a3bd5
1000ch <shogosensui@gmail.com>
2020-10-04 20:40:19 +0900

    refactor: migrate postcss to v8

--- index.js
+++ index.js
@@ -2,16 +2,21 @@
 const postcss = require('postcss');
 const CSSComb = require('csscomb');
 
-module.exports = postcss.plugin('postcss-csscomb', options => {
+module.exports = (options = {}) => {
   const optionType = typeof options;
   if (optionType !== 'object' && optionType !== 'string') {
     options = {};
   }
 
-  return async (root, result) => {
-    const {css} = root.source.input;
-    const csscomb = new CSSComb(options);
-    const sorted = await csscomb.processString(css);
-    result.root = postcss.parse(sorted);
+  return {
+    postcssPlugin: 'postcss-csscomb',
+    async Once(root, {result}) {
+      const {css} = root.source.input;
+      const csscomb = new CSSComb(options);
+      const sorted = await csscomb.processString(css);
+      result.root = postcss.parse(sorted);
+    }
   };
-});
+};
+
+module.exports.postcss = true;
